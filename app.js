398eaaf385f98688cef045bd3aef55a9141edcd9
Hudebiya Bano <HudebiyaBano@gmail.com>
2026-07-15 21:59:57 +0500

    Blog Page

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ function deletePost(){
 }
 function editPost(){
     var card = event.target.parentNode.parentNode
-    var title =card.children[1].children[0].children[0].children[0].innerText
-    var description =card.children[1].children[0].children[1].innerText
+    var title = card.querySelector("h4").innerText
+    var description = card.querySelector("p").innerText
     document.getElementById("title").value = title
     document.getElementById("description").value = description
     card.remove()
@@ -28,27 +28,49 @@ function post(){
     console.log(title.value , description.value);
     var posts = document.getElementById("posts")
    if(title.value.trim() && description.value.trim()){
-     posts.innerHTML += `
-     <div class="card mb-2">
-              <div class="card-header">~Post</div>
-              <div style="background-image:url(${cardBg})" class="card-body">
-                <figure>
-                  <blockquote class="blockquote">
-                    <p>
-                      ${title.value}
-                    </p>
-                  </blockquote>
-                  <figcaption class="blockquote-footer">
-                    ${description.value}
-                  </figcaption>
-                </figure>
-              </div>
-              <div class="ms-auto m-2">
-              <button onclick="editPost()" class="btn btn-success">Edit</button>
-              <button onclick="deletePost()" class="btn btn-danger">Delete</button>
-              </div>
-            </div>
-    `
+    posts.innerHTML += `
+<div class="card mb-3">
+
+    <div class="card-header text-center">
+         Post
+    </div>
+
+    <div
+        style="
+            background-image:url(${cardBg});
+            height:220px;
+            background-size:cover;
+            background-position:center;
+        ">
+    </div>
+
+    <div class="card-body">
+
+        <h4 class="fw-bold">
+            ${title.value}
+        </h4>
+
+        <p class="text-muted">
+            ${description.value}
+        </p>
+
+    </div>
+
+    <div class="d-flex justify-content-center gap-2 mb-3">
+
+        <button onclick="editPost()" class="btn btn-success">
+            Edit
+        </button>
+
+        <button onclick="deletePost()" class="btn btn-danger">
+            Delete
+        </button>
+
+    </div>
+
+</div>
+`
+
    }else{
     Swal.fire({
   icon: "error",
